f9b995590f0bc1c76c8ec264fdbb908754892d2d
Carlo Alberto Ferraris <cafxx@strayorange.com>
2019-05-23 15:25:14 +0900

    modernize

--- rtm.go
+++ rtm.go
@@ -3,36 +3,49 @@ package rtm
 
 // TxBegin marks the start of transaction. It will return a status code
 // different to TxBeginStarted when the transaction fails.
-func TxBegin() (status uint32)
+func TxBegin() TxBeginStatus {
+	s := txBegin()
+	return {Status(s>>24), s&0xFF}
+}
 
-// TxAbort aborts transaction
-func TxAbort()
+func txBegin() (status uint32)
+
+// TxAbort aborts transaction with the provided reason, that will
+// be available in the TxAbortExplicitReason field of the
+// TxBeginStatus returned by TxBegin
+func TxAbort(reason uint8)
 
 // TxEnd marks the end of transaction
 func TxEnd()
 
-// TxTest returns uint8(1) if the processor is executing a transactional region.
-func TxTest() (status uint8)
-
-// GetImm returns uint8 value from the uint32 status returned by TxBegin
-func GetImm(status uint32) uint8 {
-	return uint8(((status) >> 24) & 0xff)
+// InTx returns true if the processor is executing a transactional region.
+func InTx() bool {
+	return txTest() == 1
 }
+
+func txTest() (status uint8)
+
+type Status uint8
 
 const (
 	// TxBeginStarted is returned by TxBegin() when transaction is started
-	TxBeginStarted uint32 = ^uint32(0)
+	TxBeginStarted Status = 0
 	// TxAbortExplicit bit is set if abort caused by explicit abort instruction.
-	TxAbortExplicit uint32 = (1 << 0)
+	TxAbortExplicit = 1 << iota
 	// TxAbortRetry bit is set if the transaction may succeed on a retry
-	TxAbortRetry uint32 = (1 << 1)
+	TxAbortRetry
 	// TxAbortConflict bit is set if another logical processor triggered a
 	// conflict with a memory address that was part of the transaction
-	TxAbortConflict uint32 = (1 << 2)
+	TxAbortConflict
 	// TxAbortCapacity bit is set if RTM buffer overflowed
-	TxAbortCapacity uint32 = (1 << 3)
+	TxAbortCapacity
 	// TxAbortDebug is set if debug breakpoint triggered
-	TxAbortDebug uint32 = (1 << 4)
+	TxAbortDebug
 	// TxAbortNested is set if abort occurred in a nested transaction
-	TxAbortNested uint32 = (1 << 5)
+	TxAbortNested
 )
+
+type TxBeginStatus struct {
+	Status Status
+	TxAbortExplicitReason uint8
+}
